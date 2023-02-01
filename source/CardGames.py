7d415669aa23bea389da6776f868a8aec85a8af3
Jonathan Blankinship <jonny.blankinship@gmail.com>
2023-01-31 19:01:14 -0800

    multiple player display

--- source/CardGames.py
+++ source/CardGames.py
@@ -116,6 +116,10 @@ class Player:
     self.hand = []
     self.knownCards = []
 
+  def printMult(self, players):
+    for p in players:
+      p.display()
+    
 class Dealer:
   def __init__(self, deck: Deck):
     self.deck = deck
