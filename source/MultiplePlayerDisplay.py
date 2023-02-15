b2424b3adba6216f99d1b16c1435f4f66ae44621
Andre Davidian <andredavidian@Ardas-MacBook-Air.local>
2023-02-14 22:13:34 -0800

    feature/Multiple-player-display

---
+++ source/MultiplePlayerDisplay.py
@@ -0,0 +1,12 @@
+def printHands(players: dict):
+    # Show each player's hand
+    for player in players.values():
+        print(f'{player.name}:')
+        player.showHand(True)
+        print() 
+
+
+
+     
+            
+        
