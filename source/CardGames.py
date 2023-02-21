54a9b2b80a123e7c84f71295e7ae8960f0b3430a
Alex-Church <122952898+Alex-Church@users.noreply.github.com>
2023-02-20 21:03:12 -0800

    Hit_Stand

--- source/CardGames.py
+++ source/CardGames.py
@@ -187,3 +187,16 @@ class Dealer:
   def resetDeck(self):
     self.deck.reset()
     self.deck.shuffle()
+
+
+
+
+  def Hit_Stand(self, input, hand):
+    HitStand_input = input("Hit or Stand")
+
+    if HitStand_input == "Hit":
+        hand = hand.addCard():
+        return hand
+    
+    elif HitStand_input == "Stand":
+        return hand
