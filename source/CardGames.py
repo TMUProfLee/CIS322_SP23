47f4bf11a09584fc64f248d8f2c301c100b7ef6b
Andre Davidian <davidianaa@masters.edu>
2023-02-21 21:16:41 -0800

    feature/player_age

--- source/CardGames.py
+++ source/CardGames.py
@@ -156,3 +156,27 @@ class Dealer:
   def resetDeck(self):
     self.deck.reset()
     self.deck.shuffle()
+
+def printHands(players: dict):
+    # Show each player's hand
+    for player in players.values():
+        print(f'{player.name}:')
+        player.showHand(True)
+        print()
+
+def player_ages(players):
+    playerAge = []
+    for player in players.values():
+        while True:
+            try:
+                age = int(input("Enter {}'s age: ".format(player.name)))
+                playerAge.append((player, age))
+                break
+            except ValueError:
+                print("Please enter a valid age: ")
+    playerAge = sorted(playerAge, key=lambda x: x[1])
+    youngestPlayer = playerAge[0][0]
+    print("{} goes first!\n".format(youngestPlayer.name))
+    return [p for p, _ in playerAge]
+
+      
