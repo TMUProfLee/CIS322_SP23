7d39bb0edb993e723954b023d021a0f41410a1d3
allyniti <119133399+allyniti@users.noreply.github.com>
2023-02-21 21:20:20 -0800

    go fish class

--- source/CardGames.py
+++ source/CardGames.py
@@ -1,6 +1,7 @@
 import random
 import os
 import sys
+import time
 sys.path
 
 cardImages = []
@@ -26,6 +27,8 @@ class Card:
     
   def __str__(self):
     return f'{self.value}'
+
+  
 
 
   def __eq__(self, other):
@@ -98,6 +101,8 @@ class Player:
     self.knownCards = []
     self.money = money
     
+  def __str__(self):
+    return (str(self.name))
 
   def addMoney(self, amount: int):
     self.money += amount
@@ -150,6 +155,16 @@ class Player:
     self.knownCards = []
   
   ##MY FUNCTION##
+
+  def printHand(self):
+    list1 = []
+    for i in self.hand:
+      list1.append(str(i))
+    return list(map(int, list1))
+
+  def get_playerHand(self):
+    print()
+
   def addValues(self):
     list1 = []
     #Creates empty list
@@ -162,14 +177,6 @@ class Player:
     #converts all strings in list into integer
     return sum(integer_list)
 
-    #if self.name[-1] == 's':
-      #if the player name ends in S (kinda irrelevant)
-      #return print("\nValue of all cards in %s' hand is" % self.name, sum(integer_list))
-      #sum(integer_list) adds all the values in the players hand
-      
-    
-    #else:
-      #return print("\nValue of all cards in %s's hand is" % self.name, sum(integer_list))
 
 
 class Dealer:
@@ -196,8 +203,104 @@ class Dealer:
         player.addCard(self.deck.getCard())
     return True
 
+  #def askCard(self, player_turn, player_asked, value, playerDeckinfo, players: "list[Player]"):
+    
+
+
   def resetDeck(self):
     self.deck.reset()
     self.deck.shuffle()
+
+class GoFish:
+  def __init__(self, deck: Deck, players: "list[Player]"):
+    self.deck = deck
+    self. dealer = dealer
+
+  def start_game(self, players):
+    deck = Deck()
+    dealer = Dealer(deck)
+    dealer.dealCards(5, players)
+    for player in players:
+      player.showHand(player.hand)
+  
+  #adds name and deck to a list
+  def create_deckInfo(self, players):
+    playerDeckInfo = []
+    for i in range(len(players)):
+      playerDeckInfo.append([])
+  
+    #player names and deck/hand
+    for i in range(len(players)):
+      playerDeckInfo[i].append(players[i])
+      playerDeckInfo[i].append(players[i].hand)
+    return playerDeckInfo
+
+  #gets index of name or card wanted
+  def index(self, players, get_player_index):
+    info = GoFish.create_deckInfo(self, players)
+    for i in range(len(GoFish.create_deckInfo(self, players))):
+      player = info[i][0]
+      if str(get_player_index) == str(player):
+        index_value = i
+        return info[index_value]
+
+  #when you have the card 
+  def surrender_card(self, value_wanted, suit_wanted, p_turn, p_asked):
+    list1 = [] 
+    list2 = []
+    counter = 0 
+    #get_val
+    #if value asked for is not in player deck
+    for i in p_asked[1]:
+      print(i.suit)
+      print("(same value:", bool(str(i) == str(value_wanted)),")", "(same suit:",bool(i.suit == suit_wanted),")")
+      if str(i) != str(value_wanted):
+        counter += 1
+      elif i.suit != str(suit_wanted):
+        counter += 1
+      
+    #if it is, it will add that card to the player who asked and remove 
+    #card from player who has to turn it in 
+      else:
+        #appends the card 
+        list1.append(p_asked[1][counter])
+        p_turn[0].addCard(list1[0])
+      
+        #pop from player deck to see if they have card
+        #if its not the value the other player asked for
+        #it adds it to a temp list
+        #if it is, it does not add it to the temp list
+
+        for i in range(len(p_asked[1])):
+          a = p_asked[1].pop()
+          #print(i, str(a), str(value_wanted))
+          #print(bool(str(a) == str(value_wanted)), bool(a.suit == suit_wanted))
+          if str(a) != str(value_wanted):
+            list2.append(a)
+          elif a.suit != str(suit_wanted):
+            list2.append(a)
+
+
+        #transfers temp list to the player deck 
+        for i in range(len(list2)):
+          p_asked[1].append(list2[i])
+        
+        print(p_asked[1])
+        p_turn[0].showHand()
+        p_asked[0].showHand()
+        return 
+
+  def ask_Card(self, value_wanted, suit_wanted, players, player_turn):
+    #player_turn = GoFish.index(self, players, player_turn)
+    player_asked = input("Who do you want to ask?: ")
+
+    value_player_asked = GoFish.index(self, players, player_asked)
+    value_player_turn = GoFish.index(self, players, "Camila")
+    #print(value_player_turn)
+
+    GoFish.surrender_card(self, value_wanted, suit_wanted, value_player_turn, value_player_asked)
+
+
+    
 
 
