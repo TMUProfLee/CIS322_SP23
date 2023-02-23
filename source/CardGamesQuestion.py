603d655a755c297e06d98cb8257c1dab9413cd45
zwilson1301 <wilfam14@gmail.com>
2023-02-22 16:10:01 -0800

    questions added

---
+++ source/CardGamesQuestion.py
@@ -0,0 +1,204 @@
+
+import random
+import os
+import time
+cardImages = []
+values = list(range(1,14))
+suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+def find_root_dir():
+  cwd = os.getcwd()
+  while 'source' not in os.listdir():
+    os.chdir('..')
+    cwd = os.path.join( cwd, '..')
+  return cwd
+
+class Card:
+  def __init__(self, suit, value, image, cardBack):
+    self.cardBack = cardBack
+    self.suit = suit
+    self.value = value
+    self.image = image
+    self.shortImage = []
+    for line in self.image:
+      self.shortImage.append(line[:4])
+
+  def __eq__(self, other):
+    if not type(other) == Card:
+      return False
+    return self.suit == other.suit and \
+      self.value == other.value
+  def __str__(self):
+    return str(self.value)
+class Deck:
+  def __init__(self):
+    root_dir = os.path.join( find_root_dir(), 'source')
+    cards_file = f'{root_dir}{os.path.sep}playing_cards.txt'
+    with open(cards_file, "r") as cards:
+      cardBack = []
+      for _ in range(6):
+        line = cards.readline()
+        cardBack.append(line.replace("\n",""))
+      card = []
+      level = 0
+      for line in cards.readlines():
+        if len(line) == 1:
+          cardImages.append(card)
+          level = 0
+          card = []
+          continue
+        card.append(line.replace("\n",""))
+        level += 1
+      cardImages.append(card)
+    
+    deck = []
+    index = 0
+    for suit in suits:
+      for value in values:
+        deck.append(Card(suit, value, cardImages[index], cardBack))
+        index += 1
+    
+    self.cards = deck
+    self.size = len(deck)
+    self.cardBack = cardBack
+    self.discarded = []
+
+  def reset(self):
+    self.cards += self.discarded
+    self.discarded = []
+    self.size = len(self.cards)
+
+  def shuffle(self):
+    random.shuffle(self.cards)
+
+  def getCard(self):
+    card = self.cards.pop()
+    self.size -= 1
+    self.discarded.append(card)
+    return card
+
+class Player:
+  def __init__(self, name, money: int = 0):
+    self.name = name
+    self.hand = []
+    self.knownCards = []
+    self.money = money
+
+  def addMoney(self, amount: int):
+    self.money += amount
+    return self.money
+
+  def makeBet(self, amount: int):
+    if amount > self.money:
+      print("%s does not have enough money to make this bet." % self.name)
+      return self.money
+    self.money -= amount
+    return self.money
+
+  def addCard(self, card: Card, isKnown: bool = True):
+    self.hand.append(card)
+    if isKnown:
+      self.knownCards.append(True)
+    else:
+      self.knownCards.append(False)
+
+  def showHand(self, printShort: bool = False):
+    for idx in range(6):
+      for i, card in enumerate(self.hand):
+        if printShort and i < len(self.hand)-1:
+          image = card.shortImage[idx]  if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+      print()
+  def High(self):
+    l =[]
+    l2 =[]
+    for c in self.hand:
+      l.append(str(c))
+    for c in l:
+      l2.append(int(c))
+    x = max(l2)
+    print(x)
+
+  def clearHand(self):
+    self.hand = []
+    self.knownCards = []
+
+class Dealer:
+  def __init__(self, deck: Deck):
+    self.deck = deck
+    self.deck.shuffle()
+
+  def printCards(self, cards: "list[Player]", showFront: bool, printShort: bool = True):
+    for idx in range(6):
+      for i, card in enumerate(cards):
+        if printShort and i < len(cards)-1:
+          image = card.shortImage[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+      print()
+
+  def dealCards(self, numCards: int, players: "list[Player]"):
+    if numCards * len(players) > self.deck.size:
+      return False
+    for player in players:
+      for _ in range(numCards):
+        player.addCard(self.deck.getCard())
+    return True
+
+  def resetDeck(self):
+    self.deck.reset()
+    self.deck.shuffle()
+
+def question():
+  x = input('It is your turn who whould you like to ask? ')
+  y = input('what card would you like to ask that player for?')
+  if x == 'p1':
+    print('pass the computer to p1')
+    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+    time.sleep(10) 
+    print('do you have', y, "?")
+    z = input('do you have one?')
+    if y == 'yes':
+      #give card
+      print('player has card')
+    elif y == 'no':
+      print('Go fish')
+  if x == 'p2':
+    print('pass the computer to p2')
+    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+    time.sleep(10) 
+    print('do you have', y, "?")
+    z = input('do you have one?')
+    if y == 'yes':
+      #give card
+      print('player has card')
+    elif y == 'no':
+      print('Go fish')
+  if x == 'p3':
+    print('pass the computer to p3')
+    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+    time.sleep(10) 
+    print('do you have', y, "?")
+    z = input('do you have one?')
+    if y == 'yes':
+      #give card
+      print('player has card')
+    elif y == 'no':
+      print('Go fish')
+  if x == 'p4':
+    print('pass the computer to p4')
+    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+    time.sleep(10) 
+    print('do you have', y, "?")
+    z = input('do you have one?')
+    if y == 'yes':
+      #give card
+      print('player has card')
+    elif y == 'no':
+      print('Go fish')
+question()
