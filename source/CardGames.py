b70296b0c4c0b07c73815ab3f056e1aa046ca9f8
Robert Rueda <robertr@Roberts-MacBook-Pro.local>
2023-02-22 16:27:34 -0800

    count cards function

--- source/CardGames.py
+++ source/CardGames.py
@@ -89,6 +89,14 @@ def getCard( suit, value):
       return card
   return None
 
+####################################################################################
+def count_cards(deck):
+  #How many cards are in the deck assigned to "num_cards"
+  num_cards = len(deck)
+  #For each iteration the number of cars in the deck decrease by 1 card
+  for i in range(num_cards):
+    print("There are "+ str(num_cards - i) +" more cards in the deck")
+#####################################################################################
 class Player:
   def __init__(self, name, money: int = 0):
     self.name = name
