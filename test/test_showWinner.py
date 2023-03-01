a86106e961df0240a29413f8bce111c243e28e08
dhale27 <donovanhale5@gmail.com>
2023-02-28 17:47:05 -0800

    Sprint 2 - Adding showWinner Function

---
+++ test/test_showWinner.py
@@ -0,0 +1,26 @@
+from testing_base import *
+
+def test_showWinner():
+
+    # Create two players
+    donovan = Player('Donovan')
+    jesse = Player('Jesse')
+    players = [donovan, jesse]
+
+    # Add cards to players' hand
+    for p in players: 
+        for i in range(6):
+            p.addCard(Card("Hearts", random.randint(1, 10), [], []))
+
+    for p in players:
+        print(p.showValue(), p.name)
+    
+    # Determine the winner using the showWinner function
+    winner = showWinner(players)
+    
+    # Test that the winner is correct using assert
+    if winner == donovan:
+        assert winner.getName() == 'Donovan'
+    elif winner == jesse:
+        assert winner.getName() == 'Jesse'
+    print (winner)
