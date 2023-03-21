40cf368c5b04ced9c22f9484ab8924f05fa6ad63
Alex-Church <122952898+Alex-Church@users.noreply.github.com>
2023-03-20 20:38:04 -0700

    Update Hit:Stand.py

--- source/Hit:Stand.py
+++ source/Hit:Stand.py
@@ -1,10 +1,17 @@
+def Hit_Stand(self, Player):
+  def_input = input("Would you like to Hit or Stand?  ")
+    while def_input not in ["Hit", "Stand"]:
+        print("Invalid input. Please enter 'Hit' or 'Stand'.")
+        def_input = input("Would you like to Hit or Stand?  ")
 
-def Hit_Stand(self, input, hand):
-    HitStand_input = input("Hit or Stand")
+    if def_input == 'Hit':
+        Player = Player.deck.addCard()
+        return Player
+    elif def_input =='hit':
+        Player = Player.deck.addCard()
+        return Player
 
-    if HitStand_input == "Hit":
-        hand = hand.addCard():
-        return hand
-    
-    elif HitStand_input == "Stand":
-        return hand
+    elif def_input == "Stand":
+        return Player
+    elif def_input == "stand":
+        return Player
