d3cd055c5053f2783a4f16ba701103ad8887f239
allyniti <119133399+allyniti@users.noreply.github.com>
2023-03-31 13:14:22 -0700

    test

---
+++ test/test_allfeatures.py
@@ -0,0 +1,26 @@
+from testing_base import *
+import random
+
+def test_game_over():
+    camila = Player('Camila')
+    kaylie = Player('Kaylie')
+    john = Player('John')
+    players = [camila, kaylie, john]
+
+    game = GoFish(players)
+    deck = Deck()
+    dealer = Dealer(deck)
+    dealer.dealCards(5, players[0:3])
+    #when asked for name, choose Camila or Kaylie
+    #John's deck is empty so it will say "Game over"
+    #when a player takes another players last card, the game is over.
+    
+    for player in players:
+      player.showHand(player.hand)
+    for i in range (200):
+      Player.printCardsIfSet(camila)
+      game.ask_card()
+      
+
+    
+test_game_over()
