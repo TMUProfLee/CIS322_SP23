bf869ae076ea70439cd2d37090eef130681cf726
zwilson1301 <wilfam14@gmail.com>
2023-04-04 19:33:19 -0700

    2.0

--- test/test_StartScreen.py
+++ test/test_StartScreen.py
@@ -4,5 +4,5 @@ def test_start_game(capsys, monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: '2')
     start_game()
     captured = capsys.readouterr()
-    expected_output = "**************************\n*                        *\n*  Welcome to My Game!   *\n*                        *\n**************************\n\nHow many players would you like to have? "
+    expected_output = "**************************\n*                        *\n*  Welcome to Go Fish    *\n*                        *\n**************************\n\n"
     assert captured.out == expected_output
