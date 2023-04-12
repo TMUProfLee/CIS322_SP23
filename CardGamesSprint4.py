78f71e37a364e8bc250ff2304cd759f6bbe48bac
Robert Rueda <rrueda@mastesr.edu>
2023-04-11 23:29:16 -0700

    says go fish

---
+++ CardGamesSprint4.py
@@ -0,0 +1,166 @@
+
+import random
+import os
+
+cardImages = []
+values = list(range(1,14))
+suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+def find_root_dir():
+  cwd = os.getcwd()
+  while 'source' not in os.listdir():
+    os.chdir('..')
+    cwd = os.path.join( cwd, '..')
+  return cwd
+
+class Card:
+  def __init__(self, suit, value, image, cardBack):
+    self.cardBack = cardBack
+    self.suit = suit
+    self.value = value
+    self.image = image
+    self.shortImage = []
+    for line in self.image:
+      self.shortImage.append(line[:4])
+
+  def __eq__(self, other):
+    if not type(other) == Card:
+      return False
+    return self.suit == other.suit and \
+      self.value == other.value
+
+class Deck:
+  def __init__(self):
+    root_dir = os.path.join( find_root_dir(), 'source')
+    cards_file = f'{root_dir}{os.path.sep}playing_cards.txt'
+    with open(cards_file, "r") as cards:
+      cardBack = []
+      for _ in range(6):
+        line = cards.readline()
+        cardBack.append(line.replace("\n",""))
+      card = []
+      level = 0
+      for line in cards.readlines():
+        if len(line) == 1:
+          cardImages.append(card)
+          level = 0
+          card = []
+          continue
+        card.append(line.replace("\n",""))
+        level += 1
+      cardImages.append(card)
+    
+    deck = []
+    index = 0
+    for suit in suits:
+      for value in values:
+        deck.append(Card(suit, value, cardImages[index], cardBack))
+        index += 1
+    
+    self.cards = deck
+    self.size = len(deck)
+    self.cardBack = cardBack
+    self.discarded = []
+
+  def reset(self):
+    self.cards += self.discarded
+    self.discarded = []
+    self.size = len(self.cards)
+
+  def shuffle(self):
+    random.shuffle(self.cards)
+
+  def getCard(self):
+    card = self.cards.pop()
+    self.size -= 1
+    self.discarded.append(card)
+    return card
+####################################################################################
+  def count_cards(deck):
+  #How many cards are in the deck assigned to "num_cards"
+  num_cards = len(deck)
+  #For each iteration the number of cars in the deck decrease by 1 card
+  for i in range(num_cards):
+    print("There are "+ str(num_cards - i) +" more cards in the deck")
+#when the deck reaches 0, then the game is over
+  if num_cards == 0:
+      print("The deck is now empty. Game over, Thanks for playing!")
+
+  def sayGoFish():
+    response = input("Do you have any of the requested cards? (YES/NO): ")
+    if response.upper() == "NO":
+        return "GO FISH!!"
+    elif response.upper() == "YES":
+        return ""
+    else:
+        return "Looks like there was a typo in your response, please try again"
+
+#####################################################################################
+
+class Player:
+  def __init__(self, name, money: int = 0):
+    self.name = name
+    self.hand = []
+    self.knownCards = []
+    self.money = money
+
+  def addMoney(self, amount: int):
+    self.money += amount
+    return self.money
+
+  def makeBet(self, amount: int):
+    if amount > self.money:
+      print("%s does not have enough money to make this bet." % self.name)
+      return self.money
+    self.money -= amount
+    return self.money
+
+  def addCard(self, card: Card, isKnown: bool = True):
+    self.hand.append(card)
+    if isKnown:
+      self.knownCards.append(True)
+    else:
+      self.knownCards.append(False)
+
+  def showHand(self, printShort: bool = False):
+    for idx in range(6):
+      for i, card in enumerate(self.hand):
+        if printShort and i < len(self.hand)-1:
+          image = card.shortImage[idx]  if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
+          print(image, end="")
+      print()
+
+  def clearHand(self):
+    self.hand = []
+    self.knownCards = []
+
+class Dealer:
+  def __init__(self, deck: Deck):
+    self.deck = deck
+    self.deck.shuffle()
+
+  def printCards(self, cards: "list[Player]", showFront: bool, printShort: bool = True):
+    for idx in range(6):
+      for i, card in enumerate(cards):
+        if printShort and i < len(cards)-1:
+          image = card.shortImage[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+        else:
+          image = card.image[idx] if showFront else card.cardBack[idx]
+          print(image, end="")
+      print()
+
+  def dealCards(self, numCards: int, players: "list[Player]"):
+    if numCards * len(players) > self.deck.size:
+      return False
+    for player in players:
+      for _ in range(numCards):
+        player.addCard(self.deck.getCard())
+    return True
+
+  def resetDeck(self):
+    self.deck.reset()
+    self.deck.shuffle()
