38ac863ec6f8351dee766a40018b886783f977fb
allyniti <119133399+allyniti@users.noreply.github.com>
2023-04-18 21:29:23 -0700

    game loop and fix surrender card

--- source/CardGames.py
+++ source/CardGames.py
@@ -34,6 +34,7 @@ class Card:
       self.value == other.value
   def __str__(self):
     return str(self.value)
+  
 class Deck:
   def __init__(self):
     root_dir = os.path.join( find_root_dir(), 'source')
@@ -67,6 +68,30 @@ class Deck:
     self.cardBack = cardBack
     self.discarded = []
 
+  def count_cards(deck):  #How many cards are in the deck assigned to "num_cards"  
+    num_cards = deck.size()
+    #For each iteration the number of cars in the deck decrease by 1 card  
+    for i in range(num_cards):    
+      print("There are "+ str(num_cards - i) +" more cards in the deck")
+      #when the deck reaches 0, then the game is over  
+      if num_cards == 0:      
+        print("The deck is now empty. Game over, Thanks for playing!")  
+  
+####################################################################################
+  def drawCard(self):
+      """
+      Draw a card from the top of the deck.
+      Returns:
+        Card: The card drawn from the top of the deck.
+      """
+      if self.size > 0:
+        card = self.getCard()
+        print(f"Card drawn: {card.suit} {card.value}")
+        return card
+      else:
+        print("No cards left in the deck.")
+        return None
+      
   def reset(self):
     self.cards += self.discarded
     self.discarded = []
@@ -95,7 +120,7 @@ def count_cards(deck):
   num_cards = len(deck)
   #For each iteration the number of cars in the deck decrease by 1 card
   for i in range(num_cards):
-    print("There are "+ str(num_cards - i) +" more cards in the deck")
+    print("There are "+ str(num_cards) +" more cards in the deck")
 #####################################################################################
 class Player:
   def __init__(self, name, money: int = 0):
@@ -238,42 +263,95 @@ class GoFish:
   def __init__(self):
     self.playerinfo = []
     self.players = []
-    self.value_wanted = int
-    self.suit_wanted = ""
-    self.player_asked = ""
-    self.player_turn = ""
-    self.info_player_turn = int
-    self.info_player_asked = int
+    self.player_dict = {}
+    self.value_wanted = 0
+    self.player_asked = None
+    self.player_turn = None
+    self.info_player_turn = 0
+    self.info_player_asked = 0
+    self.deck = Deck()
+    self.dealer = Dealer(self.deck)
+    self.width = os.get_terminal_size().columns
 
-  def multiplayer_info(self):
-    #for print statements and centering
+  def start_game(self):
+    
+    print("*._."*(self.width//4))
+    print()
+    print("Welcome to Go Fish!".center(self.width))
+    print()
+    print("*._."*(self.width//4))
+
+    player_number = self.multiplayer_info()
+    self.point_tracker_interface(player_number)
+
+    
+    self.dealer.dealCards(5, self.players)
+    for player in self.players:
+      print("\n",player)
+      player.showHand(player.hand)
+
+    self.game_loop()
+
+  def game_loop(self):
+      repeat = True
+      while self.count_cards() != True:
+          for key in self.players:
+            turn_repeat = self.ask_card(key)
+            while turn_repeat == "go_again":
+              turn_repeat = self.ask_card(key)
+
+              print()
+
+  def ask_card(self, player_turn):
     width = os.get_terminal_size().columns
+    deco = "*-"*(width//2)
+
+    self.player_deck_info()
+    
+    
+    print(deco)
+    print()
+
+    print(("%s it's your turn!" %player_turn).center(width))
+    player_asked = input("%s who would like to ask? " % str(player_turn))
+
+    print()
+   
+    self.value_wanted = int(input("What value would you like? "))
+    self.info_player_turn = self.playerinfo[self.index(player_turn, self.playerinfo)][1]
+    self.info_player_asked = self.playerinfo[self.index(player_asked, self.playerinfo)][1]
+    self.player_turn = self.playerinfo[self.index(player_turn, self.playerinfo)][0]
+    self.player_asked = self.playerinfo[self.index(player_asked, self.playerinfo)][0]
+    turn_value = self.sayGoFish()
+      
+    self.surrender_card()
+
+    return turn_value
+    
+  def confirm_value(self):
     confirm_value = True
-
-    print()
-    print("*-"*(width//2))
-
-    #ask total num players
-    player_num = {}
-    num_players = int(input("How many players? "))
-    print()
-    print("%d players selected" %num_players, "\n")
-
-    #confirm input 
     while confirm_value:
       confirm = input("is this correct? Y/N: ")
-
       if confirm == "n":
         num_players = int(input("How many players? "))
-
       elif confirm == "y":
         confirm_value = False
-
       else:
-        #if user chooses anything other than Y?N
         print("\n[Please input Y/N]\n")
+          
+  def multiplayer_info(self):
+    width = os.get_terminal_size().columns
+    print()
+    print("*-"*(width//2))
+  
+    num_players = int(input("How many players? "))
+
+    print()
+
+    print("%d players selected" %num_players, "\n")
+
+    self.confirm_value()
         
-    #makes player object with their name
     for i in range(1, num_players+1):
       print()
       player = input("Player %d What is your name? " %i)
@@ -284,15 +362,21 @@ class GoFish:
     print("[ TOTAL NUMBER OF PLAYERS ]".center(width))
     print(str(num_players).center(width))
 
-    #adds number, player to dictionary (ex: 1, Keren) 
-    #Keren is player 1 
+    return self.assign_player_numbers()
+
+  def assign_player_numbers(self):
+    player_num = {}
+
+    youngest_player = self.playerages()
+    self.players.insert(0, self.players.pop(self.players.index(youngest_player)))
+
     for counter, player in enumerate((self.players), start =1):
       print("PLAYER %d: " % counter, player)
       player_num[counter] = player
     print()
+    self.player_dict = player_num
     return player_num
       
-  #player score will be shown here
   def point_tracker_interface(self, info):
     width = os.get_terminal_size().columns
     score = 0
@@ -307,18 +391,38 @@ class GoFish:
     print("*-"*(width//2))
 
 
+  def count_cards(self):  #How many cards are in the deck assigned to "num_cards"  
+      num_cards = self.deck.size
+      print()
+      print(" "*25, "*"*5)
+      print("cards remaining in deck: ", num_cards)
+      print(" "*25, "*"*5)
+      print()
+      #For each iteration the number of cars in the deck decrease by 1 card  
+      if num_cards == 0:
+        print("The deck is now empty. Game over, Thanks for playing!")
+        self.game_over()
+        return True
 
-  def start_game(self):
-    player_number = self.multiplayer_info()
-    self.point_tracker_interface(player_number)
 
-    deck = Deck()
-    dealer = Dealer(deck)
-    dealer.dealCards(5, self.players)
-    for player in self.players:
-      print("\n",player)
-      player.showHand(player.hand)
-    
+
+  def game_over(self):
+    width = os.get_terminal_size().columns
+    deco = "*-"*(width//2)
+    j = 0
+    gm = ["G", "A", "M", "E", "O", "V", "E","R"]
+    print(deco)
+    for i in range(width//8):
+      print("  "*j, ". *")
+      j += 3
+      if i == 10:
+        for i in range (len(gm)):
+          print("  "*j, gm[i])
+          j += 3
+    print(deco)
+    sys.exit()
+
+
   def player_deck_info(self):
     for i in range(len(self.players)):
       self.playerinfo.append([])
@@ -337,50 +441,83 @@ class GoFish:
 
   def surrender_card(self):
     temp_list1 = []
-    temp_list2 = []
     counter = 0
+    self.count_cards()
+    if len(self.info_player_asked) == 0:
+          self.game_over()
+
     for i in self.info_player_asked:
-      print(str(i), i.suit)
-      
       if str(i) != str(self.value_wanted):
         counter += 1 
-      elif i.suit != str(self.suit_wanted):
-        counter += 1
       else:
         temp_list1.append(self.info_player_asked[counter])
-        self.player_turn.addCard(temp_list1[0])
+        counter += 1 
+
+    if len(temp_list1) != 0:
+      for card in temp_list1:
+        self.player_turn.addCard(card)
+        self.info_player_asked.remove(card)
         
-        for i in range(len(self.info_player_asked)):
-          card = self.info_player_asked.pop()
-          if str(card) != str(self.value_wanted):
-            temp_list2.append(card)
-          elif card.suit != str(self.suit_wanted):
-            temp_list2.append(card)
 
-        for i in range(len(temp_list2)):
-          self.info_player_asked.append(temp_list2[i])
-
-    print(self.player_turn)
-    self.player_turn.showHand()
-    print(self.player_asked)
-    self.player_asked.showHand()
+    for player in self.players:
+      print(str(player))
+      player.showHand()
+      print()
 
 
-  def ask_card(self):
-    self.player_deck_info()
-    player_turn = input(str("What is your name? "))
-    player_asked = input(str("Who would like to ask? "))
+  def sayGoFish(self):   
+    width = os.get_terminal_size().columns
+    print()
+    deco = "*-"*(width//2)
+    response = input("Do you have any of the requested cards? (YES/NO): ")      
+    if response.upper() == "NO": 
+      self.player_turn.addCard(self.deck.getCard())
+            
+      print()
+      return print("GO FISH!!".center(width), deco)
+      
+    elif response.upper() == "YES":          
+      return "go_again"
+    else:          
+      return "Looks like there was a typo in your response, please try again"
+#self.surrender_card()
 
-    self.value_wanted = int(input("What value would you like? "))
-    self.suit_wanted = str(input("What suit would you like? "))
+  def playerages(self):
+      playerAge = []
+      for player in self.players:
+          while True:
+              try:
+                  age = int(input("Enter {}'s age: ".format(player.name)))
+                  playerAge.append((player, age))
+                  break
+              except ValueError:
+                  print("Please enter a valid age: ")
+      playerAge = sorted(playerAge, key=lambda x: x[1])
+      youngestPlayer = playerAge[0][0]
+      print("{} goes first!\n".format(youngestPlayer.name))
+      return youngestPlayer
 
-    self.info_player_turn = self.playerinfo[self.index(player_turn, self.playerinfo)][1]
-    self.info_player_asked = self.playerinfo[self.index(player_asked, self.playerinfo)][1]
+def endScreen(players):
+    import tkinter as tk
+    #Create window and greeting 
+    window = tk.Tk()
+    window.geometry("500x500")
+    greeting = tk.Label(text="Good Game! Here's the results:")
+    greeting.configure(font=("Calibri", 30), fg="green")
+    greeting.pack(pady=50)
+    #Create list of players and scores
+    player_list = tk.Listbox(window, width=50, font=("Calibri", 24), fg="gold")
+    player_list.pack(pady=20)
 
-    self.player_turn = self.playerinfo[self.index(player_turn, self.playerinfo)][0]
-    self.player_asked = self.playerinfo[self.index(player_asked, self.playerinfo)][0]
+    sorted_players = sorted(players.items(), key=lambda x: x[1], reverse=True)
 
-    self.surrender_card()
+    for player, score in sorted_players:
+        player_list.insert(tk.END, f"{player}: {score}")
+
+    window.mainloop()
+
+
+    
 def question():
   x = input('It is your turn who whould you like to ask? ')
   y = input('what card would you like to ask that player for?')
@@ -428,4 +565,3 @@ def question():
       print('player has card')
     elif y == 'no':
       print('Go fish')
-
---
+++ test/test_allfeatures.py
@@ -0,0 +1,8 @@
+from testing_base import *
+import random
+
+def all_features():
+    game = GoFish()
+    game.start_game()
+  
+all_features()
